721045142cdb3f932874fad424dadb0a7fe213d7
chortas <chortas@fi.uba.ar>
2022-06-01 19:38:24 -0300

    Create react app

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
+}
+
+export default App;
